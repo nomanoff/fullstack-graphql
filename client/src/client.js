39ee22c2fa9b7457e025dca8f5b61c40661a5728
aliabdulazeez <aliabdulazeez01@gmail.com>
2021-05-26 12:06:26 +0900

    useless

--- client/src/client.js
+++ client/src/client.js
@@ -1,5 +1,4 @@
-import { ApolloClient } from "apollo-client";
-import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { HttpLink } from "apollo-link-http";
 import gql from "graphql-tag";
 
@@ -7,12 +6,9 @@ import gql from "graphql-tag";
  * Create a new apollo client and export as default
  */
 
-const link = new HttpLink({ uri: "http://localhost:4000/" });
-const cache = new InMemoryCache();
-
 const client = new ApolloClient({
-  link,
-  cache,
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
 });
 
 export default client;
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { ApolloProvider } from "@apollo/react-hooks";
 import client from "./client";
 import App from "./components/App";
 import "./index.css";
-import ApolloClient from "apollo-client";
+import { ApolloProvider } from "@apollo/client/react";
 
 const Root = () => (
   <BrowserRouter>
